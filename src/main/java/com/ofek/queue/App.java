5b71c96c7c761c01e3e42989e3e860d7b031cf50
ofek29 <ofek.av95@gmail.com>
2025-06-23 17:28:23 +0000

    feat: run messages queue app

--- src/main/java/com/ofek/queue/App.java
+++ src/main/java/com/ofek/queue/App.java
@@ -1,13 +1,15 @@
 package com.ofek.queue;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        MessageQueue queue = new MessageQueue("messages.log");
+
+        queue.enqueue(new Message("Hello, World!"));
+
+        Message delivered = queue.dequeue();
+        System.out.println("Delivered: " + delivered);
+
+        System.out.println("Queue size: " + queue.size());
+
     }
 }
