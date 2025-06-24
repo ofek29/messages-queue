7e7f529e0531ccbbe30e14aeb185e1b86500a09b
ofek29 <ofek.av95@gmail.com>
2025-06-24 13:27:21 +0000

    feat: implement producer to produce messages

---
+++ src/main/java/com/ofek/queue/Producer.java
@@ -0,0 +1,15 @@
+package com.ofek.queue;
+
+public class Producer {
+    private final MessageQueue messageQueue;
+
+    public Producer(MessageQueue messageQueue) {
+        this.messageQueue = messageQueue;
+    }
+
+    public void produce(String payload) {
+        Message message = new Message(payload);
+        messageQueue.enqueue(message);
+    }
+
+}
